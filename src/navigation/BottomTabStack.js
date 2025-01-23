48a2a79e507e049d48e41ce6a0a1f89869d15bab
Anis Ur Rahman <78692422+AnisUrRahman1014@users.noreply.github.com>
2025-01-23 22:30:46 +0500

    Adding dependencies for google sign in

---
+++ src/navigation/BottomTabStack.js
@@ -0,0 +1,9 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const BottomTabs = createBottomTabNavigator();
+
+const BottomTabsNav = () => {
+  return <BottomTabs></BottomTabs>;
+};
+
+export default BottomTabsNav;
